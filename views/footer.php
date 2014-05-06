9d54040990c020f4eca75162f91c0aea2f6d6574
Gonzalo <gonetil@gmail.com>
2014-05-06 19:26:16 -0300

    Create footer.php

---
+++ views/footer.php
@@ -0,0 +1,17 @@
+
+	
+	<script type="text/javascript">
+	 
+		/*jQuery(function($){
+		alert($('body').parent().get(0).tagName);
+		});*/
+	</script>
+	<script src="http://ecocitizenworldmap.org/draft2/wp-content/plugins/advanced-iframe/js/ai_external.js"></script>
+
+	<?php
+	//echo $footer_block;
+	// Action::main_footer - Add items before the </body> tag
+	Event::run('ushahidi_action.main_footer');
+	?>
+</body>
+</html>
